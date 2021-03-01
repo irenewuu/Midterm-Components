acd30c81cf142dc7aec0bb26fde8d5095f2918d9
mmonay24 <marabelle.monay@gmail.com>
2021-03-01 15:57:09 -0800

    js file

---
+++ workflow.js
@@ -0,0 +1,7 @@
+
+document.querySelector(".main").innerHTML += EqualSignLayoutUI();
+document.querySelector(".main").innerHTML += EqualSignLayoutUI("3L <br>of water", "../../images/girl-smile.png", "The average amount of <br>water a person needs a <br>day:");
+document.querySelector(".main").innerHTML += ListLayoutUI();
+document.querySelector(".main").innerHTML += ListLayoutUI("2. buy clothes with <br>better quality", "../../images/medal.png", "70px");
+document.querySelector(".main").innerHTML += ListLayoutUI("3. think twice <br>before throwing out<br>your clothes", "../../images/thinker.png", "50px");
+document.querySelector(".main").innerHTML += ListLayoutUI("4. reduce the <br>amount of times you<br>do your laundry", "../../images/laundry.png", "60px");
